bc8c9a7546caa8a35394caf1910b6b1c2d897853
[Cody McKinney] <[valid-email]>
2022-10-23 00:52:00 -0700

    update to csvreader

--- src/main/java/edu/csusm/capstone/timeseriesannotator/Model/CSVReader.java
+++ src/main/java/edu/csusm/capstone/timeseriesannotator/Model/CSVReader.java
@@ -18,35 +18,51 @@ import java.util.logging.Logger;
  * @author cmcki
  */
 public class CSVReader implements DataReader {
-    
+
+    ArrayList<String[]> dataRows;
+
+    /**
+     * Class constructor.
+     */
     public CSVReader() {
-        
+
     }
-    
+
+    /**
+     *
+     * @param fileName - name of the file that has been imported
+     */
+    @Override
     public void buildDataList(String fileName) {
-            var dataRows = new ArrayList<float[]>();
-            String[] columnNames;
-            String[] values;
-            
-            try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+        dataRows = new ArrayList<String[]>();
+
+        try ( BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             int i = 0;
             while ((line = br.readLine()) != null) {
                 if (i != 0) {
-                    values = line.split(",");
-                    System.out.println(Arrays.toString(values));
+                    String[] values = line.split(",");
+                    dataRows.add(values);
                 } else {
-                columnNames = line.split(",");
-                System.out.println(Arrays.toString(columnNames));
+                    String[] columnNames = line.split(",");
+                    dataRows.add(columnNames);
                 }
                 i++;
-                if (i == 4) { break;}
+                //if (i == 4) { break;}
             }
+
+            // printing each array within ArrayList dataRows
+            for (String[] str : dataRows) {
+                System.out.println(Arrays.toString(str));
+            }
+
+            // HOW TO PRINT ONE LINE OF THE dataRows ArrayList of String arrays
+            // System.out.println(Arrays.toString(dataRows.get(4321))); 
         } catch (FileNotFoundException ex) {
             Logger.getLogger(DataReader.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(DataReader.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }

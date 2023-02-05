19b3882bd6efcfc2e2166b8540e159cd9665d95a
[Cody McKinney] <82977069+codymck@users.noreply.github.com>
2023-02-04 19:25:39 -0800

    Adding functionality for text annotations with a menu and remove feature

--- src/main/java/edu/csusm/capstone/timeseriesannotator/View/AnnotateChartPanel.java
+++ src/main/java/edu/csusm/capstone/timeseriesannotator/View/AnnotateChartPanel.java
@@ -1,11 +1,10 @@
 package edu.csusm.capstone.timeseriesannotator.View;
 
 import edu.csusm.capstone.timeseriesannotator.Model.ToolState;
-import java.awt.AWTException;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Insets;
 import java.awt.Point;
-import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -13,17 +12,23 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.List;
+import org.jfree.chart.ChartMouseEvent;
+import org.jfree.chart.ChartMouseListener;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.annotations.XYBoxAnnotation;
+import org.jfree.chart.annotations.XYTextAnnotation;
 import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.ui.RectangleEdge;
+import org.jfree.chart.ui.TextAnchor;
 
 /**
  *
  * @author Ben Theurich
+ * @author Cody McKinney
  */
 public class AnnotateChartPanel extends ChartPanel implements MouseListener {
 
@@ -81,6 +86,20 @@ public class AnnotateChartPanel extends ChartPanel implements MouseListener {
                     }
                     super.mousePressed(e);
                     break;
+                case COMMENT:
+                    if (e.getButton() == MouseEvent.BUTTON1) {
+                        CommentMenu cMenu = new CommentMenu(new javax.swing.JFrame(), true);
+                        cMenu.setVisible(true);
+                        if (cMenu.isSubmitted() == false) {
+                            return;
+                        }
+
+                        XYTextAnnotation at = new XYTextAnnotation(cMenu.getComment(), point[0], point[1]);
+                        at.setFont(new Font(cMenu.getFontName(), cMenu.getFontStyle(), cMenu.getFontSize()));
+                        at.setTextAnchor(TextAnchor.TOP_LEFT);
+                        plot.addAnnotation(at);
+                    }
+                    break;
                 default:
                     break;
             }
@@ -120,10 +139,41 @@ public class AnnotateChartPanel extends ChartPanel implements MouseListener {
                     }
                     super.mouseReleased(e);
                     break;
+                case COMMENT:
+                    if (e.getButton() == MouseEvent.BUTTON3) {
+                        removeTextAnnotation();
+                    }
+                    break;
                 default:
                     break;
             }
         }
+    }
+
+    public void removeTextAnnotation() {
+        List<XYTextAnnotation> annotations = new ArrayList<>(plot.getAnnotations());
+
+        this.addChartMouseListener(new ChartMouseListener() {
+            @Override
+            public void chartMouseClicked(ChartMouseEvent event) {
+                int x = event.getTrigger().getX();
+                int y = event.getTrigger().getY();
+                for (XYTextAnnotation annotation : annotations) {
+                    ChartRenderingInfo info = getChartRenderingInfo();
+                    Rectangle2D bounds = info.getPlotInfo().getDataArea();
+                    double x1 = plot.getDomainAxis().valueToJava2D(annotation.getX(), bounds, plot.getDomainAxisEdge());
+                    double y1 = plot.getRangeAxis().valueToJava2D(annotation.getY(), bounds, plot.getRangeAxisEdge());
+                    if (x >= x1 && x <= x1 + annotation.getText().length() * 7 && y >= y1 && y <= y1 + 20) {
+                        plot.removeAnnotation(annotation);
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void chartMouseMoved(ChartMouseEvent event) {
+            }
+        });
     }
 
     public void addRegionAnnotation() {

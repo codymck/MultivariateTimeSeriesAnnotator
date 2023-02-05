e272aaa39bc722d647e0dfd47f854c699ea98d6f
BenTheurich <bentheurich@gmail.com>
2023-02-05 15:16:16 -0800

    Adding animation for dragging shape any direction

--- src/main/java/edu/csusm/capstone/timeseriesannotator/View/RegionStruct.java
+++ src/main/java/edu/csusm/capstone/timeseriesannotator/View/RegionStruct.java
@@ -6,29 +6,29 @@ import org.jfree.chart.annotations.XYBoxAnnotation;
  * @author Ben Theurich
  */
 public class RegionStruct {
+    private double x0, y0;
     private double x1, y1;
-    private double x2, y2;
     private XYBoxAnnotation region;
     
-    RegionStruct(double[][] coordinates, XYBoxAnnotation region){
-        x1 = coordinates[0][0];
-        x2 = coordinates[1][0];
-        
-        y1 = coordinates[1][1];
-        y2 = coordinates[0][1];
+    RegionStruct(double upperLeftX, double upperLeftY, double lowerRightX, 
+        double lowerRightY, XYBoxAnnotation region){
+        x0 = upperLeftX;
+        y0 = upperLeftY;
+
+        x1 = lowerRightX;
+        y1 = lowerRightY;
         this.region = region;
     }
     
     public boolean isClickedOn(double mouseX, double mouseY){
         System.out.println("mouseX: " + mouseX + ", mouseY: " + mouseY);
-        if(mouseX >= x1 && mouseX <= x2 && 
-                mouseY >= y1 && mouseY <= y2){
+        if(mouseX >= x0 && mouseX <= x1 && 
+                mouseY >= y0 && mouseY <= y1){
             System.out.println("inside");
             return true;
         }
         return false;
     }
-    
     public XYBoxAnnotation getRegion(){
         return region;
     }

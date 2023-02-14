05b9137822fbf8dd013588e3a39e25acfc743f11
josef <josef@localhost>
2023-02-14 15:57:17 -0800

    Update Controller.java

--- src/main/java/edu/csusm/capstone/timeseriesannotator/Controller/Controller.java
+++ src/main/java/edu/csusm/capstone/timeseriesannotator/Controller/Controller.java
@@ -5,6 +5,7 @@ import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.XYPlot;
 
 /**
@@ -16,9 +17,11 @@ public class Controller {
     static AtomicReferenceArray<ArrayList<XYPlot>> atomicList = new AtomicReferenceArray<>(new ArrayList[] { new ArrayList<XYPlot>() });
     public static ArrayList<XYPlot> synced;
     static NumberAxis commonXAxis = new NumberAxis("X");
-    static NumberAxis commonYAxis = new NumberAxis("Y");
+    //static NumberAxis commonYAxis = new NumberAxis("Y");
     static AnnotateChartPanel chartPanel;
     Chart chartStruct;
+    static int counter = 0;
+    static ValueAxis FirstAxis;
     
     
     public Controller(AnnotateChartPanel cP){
@@ -29,13 +32,13 @@ public class Controller {
     public static void sync(double x1, double x2, double y1, double y2){
         //System.out.println("x1: " + x1 + " x2: " + x2);
         commonXAxis.setRange(x1,x2);
-        commonYAxis.setRange(y1,y2);
+        //commonYAxis.setRange(y1,y2);
         synced = atomicList.get(0);
         //System.out.println("Plot: " + synced.get(0) + "Size: " +  synced.size());
-        if (synced.size() > 1){
+        if (counter > 1){
             for(XYPlot x : synced){
                 x.setDomainAxis(commonXAxis);
-                x.setRangeAxis(commonYAxis);
+                //x.setRangeAxis(commonYAxis);
 //                if(!(chartPanel.isFocusOwner()) && synced.size() <= 1){
 //                    System.out.println(x.getPlotType() + " size: " +  synced.size());
 //                }
@@ -59,19 +62,25 @@ public class Controller {
         ArrayList<XYPlot> list = atomicList.get(0);
         list.add(p);
         atomicList.set(0, list);
-        //this.synced.add(p);
-        //System.out.println("Adding: " + p + "Size: " +  list.size());
         this.chartStruct = cS;
-        //Rectangle2D screenDataArea = chartPanel.getScreenDataArea();
-        //commonXAxis.setRange(screenDataArea.getMinX(),screenDataArea.getMaxX());
+        ValueAxis xAxis = p.getDomainAxis();
+        if(counter == 0){
+            FirstAxis = xAxis;
+        }
+        double x1 = xAxis.getRange().getLowerBound();
+        double x2 = xAxis.getRange().getUpperBound();
+        commonXAxis.setRange(x1,x2);
+        p.setDomainAxis(commonXAxis);
+        counter++;
     }
     
     public void removeSync(XYPlot p){
         synced.remove(p);
-        if(synced.size() < 1){
+        counter--;
+        if(counter < 1){
             for(XYPlot x : synced){
-                x.setDomainAxis(chartStruct.getDomainAxis());
-                x.setRangeAxis(chartStruct.getRangeAxis());
+                x.setDomainAxis(FirstAxis);
+                //x.setRangeAxis(chartStruct.getRangeAxis());
             }
         }
     }

418d9a1fafaed9465a60569164b5ca9f997ab143
BenTheurich <bentheurich@gmail.com>
2023-02-22 22:13:02 -0800

    fixed

--- src/main/java/edu/csusm/capstone/timeseriesannotator/View/RegionStruct.java
+++ src/main/java/edu/csusm/capstone/timeseriesannotator/View/RegionStruct.java
@@ -9,9 +9,7 @@ import org.jfree.chart.plot.IntervalMarker;
 public class RegionStruct {
     private double x0, y0;
     private double x1, y1;
-    private XYBoxAnnotation rect;
     private IntervalMarker region;
-    private String type;
     
     RegionStruct(double upperLeftX, double upperLeftY, double lowerRightX, 
         double lowerRightY, IntervalMarker region, String type){
@@ -21,34 +19,15 @@ public class RegionStruct {
         x1 = lowerRightX;
         y1 = lowerRightY;
         this.region = region;
-        this.type = type;
-    }
-    
-    RegionStruct(double upperLeftX, double upperLeftY, double lowerRightX, 
-        double lowerRightY, XYBoxAnnotation rect, String type){
-        x0 = upperLeftX;
-        y0 = upperLeftY;
-
-        x1 = lowerRightX;
-        y1 = lowerRightY;
-        this.rect = rect;
-        this.type = type;
     }
     
     public boolean isClickedOn(double mouseX, double mouseY){
         
-        if(mouseX >= x0 && mouseX <= x1){
-            if(type.equals("region")){
-                return true;
-            }else if(mouseY >= y0 && mouseY <= y1){
-                return true;
-            }
-        }
+        if(mouseX >= x0 && mouseX <= x1)
+            return true;
         return false;
     }
-    public XYBoxAnnotation getRect(){
-        return rect;
-    }
+
     public IntervalMarker getRegion(){
         return region;
     }
